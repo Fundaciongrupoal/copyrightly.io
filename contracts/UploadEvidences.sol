ef1245361666739a7f972ca8033c0231574fc21c
Roberto García <roberto@rhizomik.net>
2019-05-17 18:43:08 +0200

    Hash indexed strings using keccack256+abi.encodePackaged

--- contracts/UploadEvidences.sol
+++ contracts/UploadEvidences.sol
@@ -9,8 +9,8 @@ contract UploadEvidences {
     /// @notice Allows checking if some content has already been used as evidence.
     mapping(string => bool) private existingEvidence;
 
-    event UploadEvidenceEvent(address indexed registry, string indexed evidencedIdHash,
-        string evidenceHash, address indexed evidencer);
+    event UploadEvidenceEvent(address indexed registry, bytes32 indexed evidencedIdHash,
+        string evidencedHash, string evidenceHash, address indexed evidencer);
 
     /// @notice Get if the evidence with content hash `evidenceHash` is already registered.
     /// @param evidenceHash Hash of the evidence content, for instance IPFS Base58 Hash
@@ -18,18 +18,19 @@ contract UploadEvidences {
         return existingEvidence[evidenceHash];
     }
 
-    /// @notice Add evidence for item in `registry` identified by `evidencedId`. The evidence
-    /// has `evidenceHash` and registered by `msg.sender`.
+    /// @notice Add evidence for item in `registry` identified by `evidencedHash`. The evidence
+    /// has `evidenceHash` and is registered by `msg.sender`.
     /// @dev The address of the registry containing the evidenced item is required to update
     /// its evidence count. Evidence is stored just in the log as UploadEvidenceEvent events.
-    /// Note: the evidenceId is hashed using keccak256 before emitting the event so it can be indexed.
+    /// Note: the evidenceHash is hashed using keccak256 before emitting the event so it can be indexed.
     /// @param registry The address of the contract holding the items evidenced
-    /// @param evidencedId The identifier used by the registry contract for the item receiving evidence
+    /// @param evidencedHash Hash used by the registry contract to identify the item receiving evidence
     /// @param evidenceHash Hash of the uploaded content to be used as evidence, for instance IPFS Base58 Hash
-    function addEvidence(address registry, string memory evidencedId, string memory evidenceHash) public {
+    function addEvidence(address registry, string memory evidencedHash, string memory evidenceHash) public {
         require(!existingEvidence[evidenceHash], "The evidence has been already registered");
-        Evidencable(registry).addEvidence(evidencedId);
+        Evidencable(registry).addEvidence(evidencedHash);
         existingEvidence[evidenceHash] = true;
-        emit UploadEvidenceEvent(registry, evidencedId, evidenceHash, msg.sender);
+        emit UploadEvidenceEvent(registry, keccak256(abi.encodePacked(evidencedHash)),
+            evidencedHash, evidenceHash, msg.sender);
     }
 }
