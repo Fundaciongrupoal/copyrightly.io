0d4bfec3480bd8d92d65ed5d2e834502e38cfe05
Roberto García <roberto@rhizomik.net>
2019-05-17 18:47:22 +0200

    Avoid sleep to wait for YouTubeEvidenceEvent in tests

--- test/youtubeevidences.test.js
+++ test/youtubeevidences.test.js
@@ -2,21 +2,22 @@ const YouTubeEvidences = artifacts.require("YouTubeEvidences");
 const Manifestations = artifacts.require("Manifestations");
 const Proxy = artifacts.require("AdminUpgradeabilityProxy");
 
-contract("YouTubeEvidences - Check YouTube video ownership", function (accounts) {
+const TITLE = "Snow Againg in Mollerussa";
+const HASH1 = "QmPP8X2rWc2uanbnKpxfzEAAuHPuThQRtxpoY8CYVJxDj8";
+const HASH2 = "QmPP8X2rWc2uanbnKpxfzEAAuHPuThQRtxpoY8CYVJxdJ9";
+const HASH3 = "QmPP8X2rWc2uanbnKpxfzEAAuHPuThQRtxpoY8CYVJxdJ0";
+const VIDEO_ID1 = "ZwVNLDIJKVA";
+const VIDEO_ID2 = "3ANLBcUwizg";
+const ORACLIZE_GASLIMIT = 120000; // Based on test for gasEstimate below
+const ORACLIZE_GASPRICE = 5000000000; // 5 GWei
+const ORACLIZE_TIMEOUT = 60*1000; // 60 seconds
 
+contract("YouTubeEvidences - Check YouTube video ownership", function (accounts) {
   const OWNER = accounts[0];
   const PROXYADMIN = accounts[1];
   const MANIFESTER = accounts[2];
-  const TITLE = "Snow Againg in Mollerussa";
-  const HASH1 = "QmPP8X2rWc2uanbnKpxfzEAAuHPuThQRtxpoY8CYVJxDj8";
-  const HASH2 = "QmPP8X2rWc2uanbnKpxfzEAAuHPuThQRtxpoY8CYVJxdJ9";
-  const VIDEO_ID1 = "ZwVNLDIJKVA";
-  const VIDEO_ID2 = "3ANLBcUwizg";
-  const ORACLIZE_GASLIMIT = 100000; // Based on test for gasEstimate below
-  const ORACLIZE_GASPRICE = 5000000000; // 5 GWei
-  const ORACLIZE_DELAY = 20*1000; // 10 seconds
 
-  let evidences, proxy, manifestations, evidenceId, evidencedIdHash, evidenceVideoId, exception, price;
+  let evidences, proxy, manifestations, price;
 
   before("setup contracts for each test", async () => {
     evidences = await YouTubeEvidences.new(ORACLIZE_GASPRICE);
@@ -25,6 +26,7 @@ contract("YouTubeEvidences - Check YouTube video ownership", function (accounts)
     manifestations.addEvidenceProvider(evidences.address);
 
     await manifestations.manifestAuthorship(HASH1, TITLE, {from: MANIFESTER});
+    await manifestations.manifestAuthorship(HASH2, TITLE, {from: MANIFESTER});
 
     price = await evidences.getPrice(ORACLIZE_GASLIMIT);
     console.log("Oraclize price: " + web3.utils.fromWei(price + "") + " ether");
@@ -32,80 +34,68 @@ contract("YouTubeEvidences - Check YouTube video ownership", function (accounts)
     console.log("Custom gas price: " + ORACLIZE_GASPRICE);
   });
 
-  it("should add evidence for YouTube video linked to manifestation", async () => {
-    let eventEmitted = false;
-    await evidences.YouTubeEvidenceEvent().on("data", event => {
-      evidenceId = event.args.evidenceId;
-      evidencedIdHash = event.args.evidencedIdHash;
-      evidenceVideoId = event.args.videoId;
-      eventEmitted = true;
-    });
-
-    const originalEvidenceCount = await manifestations.getEvidenceCount(HASH1);
-
-    await evidences.check(manifestations.address, HASH1, VIDEO_ID1, ORACLIZE_GASLIMIT, {value: price});
-
-    await sleep(ORACLIZE_DELAY); // Wait for Oraclize callback
-
-    const finalEvidenceCount = await manifestations.getEvidenceCount(HASH1);
-
-    assert.equal(originalEvidenceCount.toNumber() + 1, finalEvidenceCount.toNumber(),
-      "No evidence should be accumulated");
-    assert.equal(eventEmitted, true,
-        "YouTube video linked to its manifestation should emit a YouTubeEvidenceEvent");
-    assert.equal(evidencedIdHash, web3.utils.soliditySha3(HASH1),
+  it("should add evidence for YouTube video linked to manifestation", (done) => {
+    const indexedEvidenceHash = web3.utils.soliditySha3(HASH1);
+    evidences.YouTubeEvidenceEvent(
+      {filter: {evidencedIdHash: indexedEvidenceHash}, fromBlock: "latest"})
+    .on("data", event => {
+      assert.equal(event.args.isVerified, true,
+        "YouTube video linked to its manifestation should emit a successful YouTubeEvidenceEvent");
+      assert.equal(event.args.evidencedIdHash, web3.utils.soliditySha3(HASH1),
         "unexpected evidence event hash");
-    assert.equal(evidenceVideoId, VIDEO_ID1,
-        "unexpected evidence event video id");
-  });
-
-  it("shouldn't add evidence for YouTube video not linked to manifestation", async () => {
-    let eventEmitted = false;
-    await evidences.FailedYouTubeEvidenceEvent().on("data", event => {
-      evidenceId = event.args.evidenceId;
-      evidencedIdHash = event.args.evidencedIdHash;
-      evidenceVideoId = event.args.videoId;
-      eventEmitted = true;
-    });
-
-    const originalEvidenceCount = await manifestations.getEvidenceCount(HASH1);
-
-    await evidences.check(manifestations.address, HASH1, VIDEO_ID2, ORACLIZE_GASLIMIT, {value: price});
-
-    await sleep(ORACLIZE_DELAY); // Wait for Oraclize callback
-
-    const finalEvidenceCount = await manifestations.getEvidenceCount(HASH1);
-
-    assert.equal(originalEvidenceCount.toNumber(), finalEvidenceCount.toNumber(),
-      "No evidence should be accumulated");
-    assert.equal(eventEmitted, true,
-      "YouTube video not linked to its manifestation should emit a FailedYouTubeEvidenceEvent");
-    assert.equal(evidencedIdHash, web3.utils.soliditySha3(HASH1),
+      assert.equal(event.args.evidencedHash, HASH1,
         "unexpected evidence event hash");
-    assert.equal(evidenceVideoId, VIDEO_ID2,
+      assert.equal(event.args.videoId, VIDEO_ID1,
         "unexpected evidence event video id");
-  });
-
-  it("shouldn't add evidence if manifestation does not exist", async () => {
-    let eventEmitted = false;
-    await evidences.YouTubeEvidenceEvent().on("data", event => {
-      evidenceId = event.args.evidenceId;
-      evidencedIdHash = event.args.evidencedIdHash;
-      evidenceVideoId = event.args.videoId;
-      eventEmitted = true;
+      manifestations.getEvidenceCount(HASH1).then(count => {
+        assert.equal(count.toNumber(), 1, "Evidence should have been accumulated");
+        done();
+      });
     });
+    evidences.check(manifestations.address, HASH1, VIDEO_ID1, ORACLIZE_GASLIMIT, {value: price});
+  }).timeout(ORACLIZE_TIMEOUT);
 
-    await evidences.check(manifestations.address, HASH2, VIDEO_ID1, ORACLIZE_GASLIMIT, {value: price});
+  it("shouldn't add evidence for YouTube video not linked to manifestation", (done) => {
+    const indexedEvidenceHash = web3.utils.soliditySha3(HASH2);
+    evidences.YouTubeEvidenceEvent(
+      {filter: {evidencedIdHash: indexedEvidenceHash}, fromBlock: "latest"})
+    .on("data", event => {
+      assert.equal(event.args.isVerified, false,
+        "YouTube video not linked to its manifestation should emit an unsuccessful YouTubeEvidenceEvent");
+      assert.equal(event.args.evidencedIdHash, web3.utils.soliditySha3(HASH2),
+        "unexpected evidence event hash");
+      assert.equal(event.args.evidencedHash, HASH2,
+        "unexpected evidence event hash");
+      assert.equal(event.args.videoId, VIDEO_ID2,
+        "unexpected evidence event video id");
+      manifestations.getEvidenceCount(HASH2).then(count => {
+        assert.equal(count.toNumber(), 0, "Evidence shouldn't have been accumulated");
+        done();
+      });
+    });
+    evidences.check(manifestations.address, HASH2, VIDEO_ID2, ORACLIZE_GASLIMIT, {value: price});
+  }).timeout(ORACLIZE_TIMEOUT);
 
-    await sleep(ORACLIZE_DELAY); // Wait for Oraclize callback
-
-    const finalEvidenceCount = await manifestations.getEvidenceCount(HASH2);
-
-    assert.equal(eventEmitted, false,
-      "Evidence for non-existing manifestation shouldn't emit a YouTubeEvidenceEvent");
-    assert.equal(finalEvidenceCount.toNumber(), 0,
-      "No evidences for un-existing manifestation");
-  });
+  it("shouldn't add evidence if manifestation does not exist", (done) => {
+    const indexedEvidenceHash = web3.utils.soliditySha3(HASH3);
+    evidences.YouTubeEvidenceEvent(
+      {filter: {evidencedIdHash: indexedEvidenceHash}, fromBlock: "latest"})
+    .on("data", event => {
+      assert.equal(event.args.isVerified, false,
+        "Unexisting manifestation should emit an unsuccessful YouTubeEvidenceEvent");
+      assert.equal(event.args.evidencedIdHash, web3.utils.soliditySha3(HASH3),
+        "unexpected evidence event hash");
+      assert.equal(event.args.evidencedHash, HASH3,
+        "unexpected evidence event hash");
+      assert.equal(event.args.videoId, VIDEO_ID1,
+        "unexpected evidence event video id");
+      manifestations.getEvidenceCount(HASH3).then(count => {
+        assert.equal(count.toNumber(), 0, "Evidence shouldn't have been accumulated");
+        done();
+      });
+    });
+    evidences.check(manifestations.address, HASH3, VIDEO_ID1, ORACLIZE_GASLIMIT, {value: price});
+  }).timeout(ORACLIZE_TIMEOUT);
 
   it("shouldn't add evidence if not enough ether for Oraclize call", async () => {
     exception = false;
@@ -135,9 +125,4 @@ contract("YouTubeEvidences - Check YouTube video ownership", function (accounts)
     console.log("Gas estimate: " + gasEstimate);
     assert(gasEstimate > 0, "Gas estimate for callback should be higher than 0");
   });
-
 });
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
